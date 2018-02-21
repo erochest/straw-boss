15aee6fa0ad1aeae56fbec49e4836eb3b774e3f6
Eric Rochester <erochest@gmail.com>
2018-02-21 13:43:29 -0500

    Added command-line parsing and empty modules.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#[macro_use]
+extern crate clap;
+
+use std::env;
+use std::path::PathBuf;
+use clap::Arg;
+
 fn main() {
-    println!("Hello, world!");
+    let procfile = parse_args();
+    println!("PROCFILE = {:?}", &procfile);
+}
+
+fn parse_args() -> PathBuf {
+    let matches = app_from_crate!()
+        .arg(Arg::with_name("procfile")
+            .short("p")
+            .long("procfile")
+            .value_name("FILENAME")
+            .default_value("Procfile")
+            .help("The Procfile defining the services to run locally."))
+        .get_matches();
+
+    let pwd = env::current_dir().expect("Cannot get current directory.");
+    // This should be safe to unwrap.
+    let procfile = matches.value_of("procfile").unwrap_or("Procfile");
+    pwd.join(&procfile)
 }
